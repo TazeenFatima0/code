897a829cc038bcbaa25806d4a5e8225a21ab01a6
TazeenFatima0 <tazeen0fatima@gmail.com>
2024-06-15 09:14:20 +0000

    update

---
+++ basics/datatype.js
@@ -0,0 +1,35 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3);
+console.log("tazeen")
+let a=5
+let c=8
+let d=a+c
+console.log(d);
+
+let name = "tazeen"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bitint
+//string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof"ta")    // string
+console.log(typeof 98)    // number
+console.log(typeof age); // number
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
+
+
+
+
