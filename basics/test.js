cab76be8fc611112adbf6cd5c9c6593e222ff8b7
TazeenFatima0 <tazeen0fatima@gmail.com>
2024-06-15 08:52:05 +0000

    data type

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,11 @@
-console.log("tazeen")
+
+const accountId = 14453
+let accountEmail = "tazeen@gmail.com"
+var accountPassword = "12344"
+accoutCity = "Multan"
+
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accoutCity);
