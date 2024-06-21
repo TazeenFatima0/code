ba14b7999fb8c20b3a8fa570dc080e5ca6df8c63
Tazeen Fatima <tazeen0fatima@gmail.com>
2024-06-21 04:29:45 +0000

    conversion of datatype

---
+++ basics/type.js
@@ -0,0 +1,27 @@
+let score = "5a5" // assign the value to variable
+console.log(score)// print the value of variable on screen which is store in variable named score
+
+console.log(typeof score); // here, by using the typeof checked the type of the score
+
+console.log(typeof (score))// another way to check the typeof the score
+
+let valueinnumber = Number(score) //type conversion: for converting the type of score
+console.log(valueinnumber);
+ console.log(typeof valueinnumber)
+
+// "88" => 88
+// "ss23" => Nan
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let LoggedIn = "1"
+
+let booleanLoggedIn = Boolean(LoggedIn)
+console.log(booleanLoggedIn);
+
+//1 => true; 0 => false
+// ""
