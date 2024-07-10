f8e993ae160944dad10703ab11cb47668c338865
Tazeen Fatima <tazeen0fatima@gmail.com>
2024-07-10 09:59:23 +0000

    scope

---
+++ basics/scope.js
@@ -0,0 +1,24 @@
+/*if (true) {
+const username = "Tazeen"
+if (username === "Tazeen"){
+    const website="youtube"
+    console.log(username + website);
+}
+
+console.log(username);
+
+}
+//console.log(username);*/
+
+
+// ++++++++++++++++++++++ interesting ++++++++++++++++
+
+function addone(num){
+    return num+1
+}
+ 
+console.log(addone(5))
+
+
+
+
