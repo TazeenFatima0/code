80d4f3fcda4f9cbf24d633c7f9f95eb8e564c36d
Tazeen Fatima <tazeen0fatima@gmail.com>
2024-07-10 10:45:23 +0000

    This

---
+++ basics/This.js
@@ -0,0 +1,37 @@
+/*const user = {
+    username: "Tazeen",
+    price:1000,
+    welcomemessage: function(){
+        console.log(`${this.username}, welcome to website`);
+        
+    }
+}
+user.welcomemessage()
+user.username = "sam"
+user.welcomemessage()
+console.log(this);
+*/
+
+/*function chai() {
+    let username = "Tazeen"
+    console.log(this.username);
+
+}
+
+chai()*/
+
+/*
+
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
+
+console.log(addTwo(3, 10))*/
+
+
+const addTwo = (num1, num2) => (num1 + num2)
+
+console.log(addTwo(3, 10))
+
+//implicit retun and explicit return
+
