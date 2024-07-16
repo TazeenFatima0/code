1c5d1b496898caddf3ebb30f08edc54c75863e95
Tazeen Fatima <tazeen0fatima@gmail.com>
2024-07-16 07:27:47 +0000

    update

---
+++ basics/high.js
@@ -0,0 +1,25 @@
+const coding = ["js", "rubby", "python", "cpp"]
+
+// coding.forEach( function (item) {
+
+//     console.log(item);
+// } )
+
+// coding.forEach((item) => {
+// console.log(item);
+// })
+
+// coding.forEach( (item, index, arr)=>{
+//     console.log(item, index, arr);
+// })
+
+
+
+
+
+
+
+
+
+
+
